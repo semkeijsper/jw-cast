4bd17a21a2f6dbb7f2676ef96c701c649d5a68ae
semkeijsper <78076164+semkeijsper@users.noreply.github.com>
2021-08-09 21:07:27 +0200

    add chromecast url functionality

--- src/components/VideoDialog.vue
+++ src/components/VideoDialog.vue
@@ -8,7 +8,6 @@
   >
     <v-card v-if="selectedVideo">
       <v-toolbar dense>
-        <!-- <v-toolbar-title v-text="selectedVideo.title"></v-toolbar-title> -->
         <v-spacer></v-spacer>
         <v-toolbar-items>
           <v-btn icon @click="closeDialog">
@@ -23,7 +22,7 @@
         gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
       >
         <v-card-title
-          v-text="selectedVideo.title"
+          v-text="`${selectedVideo.title} (${selectedVideo.durationFormattedHHMM})`"
           style="word-break: normal; user-select: none;"
         ></v-card-title>
       </v-img>
@@ -59,26 +58,82 @@
           </v-row>
         </v-container>
         <v-card-actions v-if="$vuetify.breakpoint.xsOnly">
-          <v-btn color="primary" dark @click="downloadSubtitles">
-            <v-icon left>
-              mdi-cast
-            </v-icon>
-            {{ translations.btnPlay }}
-          </v-btn>
+          <v-menu offset-y rounded="0">
+            <template v-slot:activator="{ attrs, on }">
+              <v-btn
+                color="primary"
+                v-bind="attrs"
+                v-on="on"
+                class="mr-2"
+                :loading="!videoMedia || !subtitleMedia"
+              >
+                <v-icon left>
+                  mdi-cast
+                </v-icon>
+                {{ translations.btnPlay }}
+              </v-btn>
+            </template>
+
+            <v-list dense v-if="videoMedia">
+              <v-list-item
+                v-for="file in videoMedia.files"
+                :key="file.checksum"
+                link
+                @click="playOnChromecast(file)"
+              >
+                <v-list-item-title v-text="file.label"></v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
         </v-card-actions>
         <v-card-actions>
           <template v-if="!$vuetify.breakpoint.xsOnly">
-            <v-btn color="primary" dark @click="downloadSubtitles">
+            <!--  -->
+            <!-- <v-btn
+              color="primary"
+              dark
+              @click="playOnChromecast"
+              :loading="!videoMedia || !subtitleMedia"
+            >
               <v-icon left>
                 mdi-cast
               </v-icon>
               {{ translations.btnPlay }}
-            </v-btn>
+            </v-btn> -->
+            <!--  -->
+            <v-menu offset-y rounded="0">
+              <template v-slot:activator="{ attrs, on }">
+                <v-btn
+                  color="primary"
+                  v-bind="attrs"
+                  v-on="on"
+                  class="mr-2"
+                  :loading="!videoMedia || !subtitleMedia"
+                >
+                  <v-icon left>
+                    mdi-cast
+                  </v-icon>
+                  {{ translations.btnPlay }}
+                </v-btn>
+              </template>
+
+              <v-list dense v-if="videoMedia">
+                <v-list-item
+                  v-for="file in videoMedia.files"
+                  :key="file.checksum"
+                  link
+                  @click="playOnChromecast(file)"
+                >
+                  <v-list-item-title v-text="file.label"></v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+            <!--  -->
             <v-spacer></v-spacer>
           </template>
           <v-menu offset-y rounded="0">
             <template v-slot:activator="{ attrs, on }">
-              <v-btn color="primary" dark v-bind="attrs" v-on="on" class="mr-2">
+              <v-btn color="primary" v-bind="attrs" v-on="on" class="mr-2" :loading="!videoMedia">
                 <v-icon left>
                   mdi-download
                 </v-icon>
@@ -86,9 +141,9 @@
               </v-btn>
             </template>
 
-            <v-list dense>
+            <v-list dense v-if="videoMedia">
               <v-list-item
-                v-for="file in selectedVideo.files"
+                v-for="file in videoMedia.files"
                 :key="file.checksum"
                 link
                 @click="downloadVideo(file)"
@@ -97,7 +152,12 @@
               </v-list-item>
             </v-list>
           </v-menu>
-          <v-btn color="primary" dark @click="downloadSubtitles">
+          <v-btn
+            color="primary"
+            @click="downloadSubtitles"
+            :loading="!subtitleMedia"
+            :disabled="!hasSubtitles"
+          >
             <v-icon left>
               mdi-download
             </v-icon>
@@ -114,6 +174,7 @@ import { Component, Vue, Watch } from 'vue-property-decorator';
 import { Getter, Mutation, State } from 'vuex-class';
 
 import { File, Language, Video } from '@/types';
+import axios from 'axios';
 
 @Component
 export default class VideoDialog extends Vue {
@@ -121,11 +182,14 @@ export default class VideoDialog extends Vue {
   videoMedia: Video | null = null;
   subtitleMedia: Video | null = null;
 
+  @State baseUrl!: string;
   @State languages!: Language[];
   @State translations!: { [key: string]: string };
+
   @State selectedVideo!: Video;
   @Mutation setSelectedVideo!: (value: Video | null) => void;
 
+  @Getter getSiteLanguage!: Language;
   @Getter getVideoLanguage!: Language;
   @Getter getSubtitleLanguage!: Language;
   @Mutation setVideoLanguage!: (value: string) => void;
@@ -141,12 +205,40 @@ export default class VideoDialog extends Vue {
   }
 
   // eslint-disable-next-line class-methods-use-this
+  playOnChromecast(file: File) {
+    const video = btoa(file.progressiveDownloadURL);
+    // sfgc: subtitle foreground color
+    let url = `https://chromecast.smplayer.info/index.php?sfgc=I2ZmZmZmZg==&url=${video}`;
+    try {
+      url += `&title=${btoa(this.selectedVideo.title.replaceAll('—', '-'))}`;
+    } catch (error) {
+      // No title then, I guess
+    }
+    if (this.hasSubtitles) {
+      const subtitles = btoa(this.subtitleUrl!);
+      url += `&subtitles=${subtitles}`;
+    }
+    window.location.href = url;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
   downloadVideo(file: File) {
     window.location.href = file.progressiveDownloadURL;
   }
 
   downloadSubtitles() {
-    window.location.href = this.selectedVideo.files[0].subtitles.url;
+    if (this.subtitleUrl === null) return;
+    window.location.href = this.subtitleUrl;
+  }
+
+  get hasSubtitles() {
+    return this.subtitleMedia?.files.some(file => file?.subtitles?.url !== undefined) ?? false;
+  }
+
+  get subtitleUrl() {
+    const found = this.subtitleMedia!.files.find(file => file?.subtitles?.url !== undefined);
+    if (found === undefined) return null;
+    return found.subtitles.url;
   }
 
   get availableLanguages() {
@@ -174,9 +266,47 @@ export default class VideoDialog extends Vue {
     this.setSubtitleLanguage(language);
   }
 
+  getMediaUrl(language: Language) {
+    return `${this.baseUrl}/media-items/${language.code}/${this.selectedVideo.languageAgnosticNaturalKey}?clientType=www`;
+  }
+
+  async loadMediaItems() {
+    if (this.videoMedia === null) {
+      [this.videoMedia] = (await axios.get(this.getMediaUrl(this.getVideoLanguage))).data.media;
+    }
+    if (this.subtitleMedia === null) {
+      [this.subtitleMedia] = (
+        await axios.get(this.getMediaUrl(this.getSubtitleLanguage))
+      ).data.media;
+    }
+  }
+
   @Watch('selectedVideo')
   onSelectedVideoChange(video: Video) {
     this.dialog = video !== null;
+    if (video !== null) {
+      this.videoMedia = null;
+      this.subtitleMedia = null;
+      if (this.getSiteLanguage.locale === this.videoLanguage) {
+        this.videoMedia = video;
+      }
+      if (this.getSiteLanguage.locale === this.subtitleLanguage) {
+        this.subtitleMedia = video;
+      }
+      this.loadMediaItems();
+    }
+  }
+
+  @Watch('videoLanguage')
+  onVideoLanguageChange() {
+    this.videoMedia = null;
+    this.loadMediaItems();
+  }
+
+  @Watch('subtitleLanguage')
+  onSubtitleLanguageChange() {
+    this.subtitleMedia = null;
+    this.loadMediaItems();
   }
 }
 </script>
